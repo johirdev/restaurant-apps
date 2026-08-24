b2752a9b1afe53baeb80fe75f9c919360cfbb462
johir-coder <88481228+johirdev@users.noreply.github.com>
2026-08-24 14:28:03 +0600

    tts

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -10,7 +10,6 @@ export default function Home() {
     <div className="">
       <BannerClient />
       <FoodCategory />
-      {/* ..dd */}
       <ShowFoodItems />
       <DiscountBanners />
       <VideoBlogSlider />
